ab4326f3ff6cdb9326924ee5f91eefd7da3cd9d2
G-0324D <byesad@gmail.com>
2022-09-28 23:00:48 +0300

    New course files.
    Codeacademy üzerinden aldığım bir eğitimin çalışma dosyası.

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
 
-export default function App() {
+export default function App(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style = {stil.container}>
+      <Text>Hello, World!</Text>
     </View>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+const stil = StyleSheet.create(
+{
+  container:{
+    flex: '1',
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+}
+);
