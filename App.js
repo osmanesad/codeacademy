bcac400adb8513c5ab30578ce8f11129fc2d854c
G-0324D <byesad@gmail.com>
2022-09-29 04:27:49 +0300

    Update - App.js
    View nesnesi üzerinde denemeler yapıyoruz.

--- App.js
+++ App.js
@@ -1,22 +1,12 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View } from "react-native";
 
-export default function App(){
-  return (
-    <View style = {stil.container}>
-      <Text>Hello, World!</Text>
-    </View>
-
-  );
-}
-
-const stil = StyleSheet.create(
-{
-  container:{
-    flex: '1',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-}
+const App = () => (
+  <>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }} />
+    <View style={{ flex: 1, width: 100, height: 100, backgroundColor: "red" }}/>
+    <View style={{ flex: 1, width: 100, height: 100, backgroundColor: "blue" }}/>
+  </>
 );
+
+export default App;
