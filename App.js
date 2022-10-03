cf0085822cd45fd548311f674140e4bf8bf1c220
G-0324D <byesad@gmail.com>
2022-10-03 05:36:43 +0300

    Update - App.js
    Kutular, kutular...

--- App.js
+++ App.js
@@ -1,24 +1,30 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 
-
-const App = () => {
-  const [name, setName] = useState('');
-
-  return (
-    <View
-      style={{ flex: 1, justifyContent: 'center', backgroundColor: '#ecf0f1' }}
-    >
-      <Text style={{ marginVertical: 16 }}>
-        { name ? 'Hi ${name}' : 'What?' }
-      </Text>
-
-      <TextInput
-        style={{ padding: 8, backgroundColor: '#f5f5f5' }}
-        onChangeText={text => setName(text)}
-      />
-    </View>
-  );
-};
+const App = () => (
+  <View style={styles.layout}>
+    <View style={styles.card} />
+    <View style={styles.card} />
+  </View>
+);
 
 export default App;
+
+export const styles = StyleSheet.create({
+  layout: {
+    flex: 1,
+    backgroundColor: '#e5e5e5',
+    justifyContent: 'center',
+  },
+  card: {
+    width: 100, 
+    height: 100, 
+    backgroundColor: 'white', 
+    margin: 16, 
+    borderRadius: 2, 
+    shadowColor: 'black', 
+    shadowOpacity: 0.3, 
+    shadowRadius: 1, 
+    shadowOffset: { height: 1, width: 0.3 } 
+  },
+});
