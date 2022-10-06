eb8a3941be12acddbd8a40a13ae9a3f3dd21e28f
G-0324D <byesad@gmail.com>
2022-10-06 20:47:13 +0300

    Update - Box
    Box nesnesine bir butonmuş gibi basılma efektini ekliyoruz.

--- App.js
+++ App.js
@@ -1,30 +1,27 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React from "react";
+import { Pressable, StyleSheet, View } from "react-native";
 
 const App = () => (
   <View style={styles.layout}>
-    <View style={styles.card} />
-    <View style={styles.card} />
+    <Pressable>{(state) => <Box pressed={state.pressed} />}</Pressable>
   </View>
 );
 
 export default App;
 
+export const Box = (props) => (
+  <View style={[styles.box, props.pressed && { backgroundColor: "blue" }]} />
+);
+
 export const styles = StyleSheet.create({
   layout: {
     flex: 1,
-    backgroundColor: '#e5e5e5',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
-  card: {
-    width: 100, 
-    height: 100, 
-    backgroundColor: 'white', 
-    margin: 16, 
-    borderRadius: 2, 
-    shadowColor: 'black', 
-    shadowOpacity: 0.3, 
-    shadowRadius: 1, 
-    shadowOffset: { height: 1, width: 0.3 } 
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: "red",
   },
 });
